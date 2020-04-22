7bae4dc883669c3952f79c716aeddf153081407b
yfwangpeng <joinlearn@163.com>
2020-04-22 21:34:56 +0800

    0422-1

--- short-url/src/main/java/com/titizz/shorturl/web/controller/IndexController.java
+++ short-url/src/main/java/com/titizz/shorturl/web/controller/IndexController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
 
@@ -26,6 +27,12 @@ public class IndexController {
     @RequestMapping(value = {"/", "index", "index.htm", "index.html", "home"})
     public String index() {
         return "index";
+    }
+
+    @RequestMapping(value = {"/index2"})
+    public ModelAndView index2() {
+        ModelAndView modelAndView = new ModelAndView("index.html");
+        return modelAndView;
     }
 
     @RequestMapping(value = "404")
